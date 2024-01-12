1913523e1e181ab9b336c07b0cf019e344958f7c
Omkar Prajapati <72141037+prajapatiomkar@users.noreply.github.com>
2024-01-12 16:07:56 +0530

    initialize client and server.

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <h1 className="flex justify-center items-center h-screen text-3xl font-bold capitalize">
+      hello from client
+    </h1>
+  );
+}
